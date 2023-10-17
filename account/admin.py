f98912faf5066b71cc24904e75bc221ea4d4b402
Shevtsov Sergey <shevadotka@gmail.com>
2023-10-17 13:34:23 +0300

    added follow user

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, Contact
 
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username','email','is_staff','is_active','photo')
+    list_display = ('username', 'email', 'is_staff', 'is_active', 'photo')
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('user_from', 'user_to', 'created')
--- account/views.py
+++ account/views.py
@@ -2,12 +2,14 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordResetConfirmView, \
     PasswordResetView
-from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
+from django.http import JsonResponse
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.contrib import messages
 
 from account.forms import CustomAuthenticationForm, UserRegistrationForm, UserUpdateForm
-from .models import CustomUser
+from .models import CustomUser, Contact
 
 
 class CustomLoginView(LoginView):
@@ -78,3 +80,23 @@ def user_list(request):
 def user_detail(request, username):
     user = get_object_or_404(CustomUser, username=username, is_active=True)
     return render(request, 'account/user/detail.html', {'section': 'people', 'user': user})
+
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = CustomUser.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user, user_to=user).delete()
+            return JsonResponse({'status': 'ok'})
+        except CustomUser.DoesNotExist:
+            return JsonResponse({'status': 'error'})
+    return JsonResponse({'status': 'error'})
