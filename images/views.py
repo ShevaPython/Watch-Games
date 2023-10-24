7bb3e008c66f4a49ade01fd7872949ce44437889
Shevtsov Sergey <shevadotka@gmail.com>
2023-10-24 16:10:48 +0300

    connect redis and create raiting and wotching with redis create view and template

--- images/views.py
+++ images/views.py
@@ -10,6 +10,9 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Image
 from .forms import ImageCreateFrom
 from actions.utils import create_action
+from WatchGames.utils_redis.redis_utils import get_redis_connection
+
+r = get_redis_connection()
 
 
 @login_required
@@ -37,8 +40,13 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    # увеличить общее число просмотров изображения на 1
+    total_views = r.incr(f'image:{image.id}:views')
+    # увеличить рейтинг изобажения на 1
+    r.zincrby('image_ranking', 1, image.id)
     return render(request, 'images/image/detail.html', {'section': 'images',
-                                                        'image': image})
+                                                        'image': image,
+                                                        'total_views': total_views})
 
 
 @login_required
@@ -88,3 +96,22 @@ def image_list(request):
                   'images/image/list.html',
                   {'section': 'images',
                    'images': images})
+
+
+@login_required
+def image_ranking(request):
+    # получить словарь рейтинга изображений
+    image_ranking = r.zrange('image_ranking', 0, -1,
+                             desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    print("Image Ranking IDs:", image_ranking_ids)
+    # получить наиболее просматриваемые изображения
+    most_viewed = list(Image.objects.filter(
+        id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    # Добавим отладочную информацию
+    print("Most Viewed Images:", most_viewed)
+    return render(request,
+                  'images/image/ranking.html',
+                  {'section': 'images',
+                   'most_viewed': most_viewed})
